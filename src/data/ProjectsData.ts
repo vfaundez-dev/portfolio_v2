42be28d826b1ed512c995feda085266014a86c22
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-01-10 18:24:45 -0300

    Changes to project data and general template adjustments

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,30 +1,30 @@
 export const projectsData = [
   {
-    title: 'Mini CRM',
-    description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
-    technologies: ['PHP', 'Laravel', 'Javascript' ,'MySQL', 'Vite'],
-    liveUrl: 'https://proyecto1.com',
-    githubUrl: 'https://github.com/usuario/proyecto1'
+    title: 'Generador de contraseñas seguras',
+    description: 'Un simple proyecto con Javascript puro, el cual genera contraseñas aleatorias seguras, indicando la longitud y si incluir numeros y símbolos.',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    liveUrl: 'https://vfh-password-generator-js.netlify.app/',
+    githubUrl: 'https://github.com/vfaundez-dev/js-password-generator'
   },
   {
-    title: 'Proyecto 2',
+    title: 'Mini CRM',
+    description: 'En etapa de finalizacion...',
+    technologies: ['PHP', 'Laravel', 'Javascript' ,'MySQL', 'Vite', 'AdminLTE'],
+    liveUrl: '',
+    githubUrl: ''
+  },
+  {
+    title: 'Proyecto 3',
     description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
     technologies: ['Vue.js', 'Express', 'PostgreSQL'],
-    liveUrl: 'https://proyecto2.com',
-    githubUrl: 'https://github.com/usuario/proyecto2'
+    liveUrl: '',
+    githubUrl: ''
   },
   {
-    title: 'Proyecto 3',
+    title: 'Proyecto 4',
     description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
     technologies: ['Javascript', 'Tailwind CSS', 'MySQL', 'MySQL', 'MySQL', 'MySQL', 'MySQL'],
-    liveUrl: 'https://proyecto2.com',
-    githubUrl: 'https://github.com/usuario/proyecto2'
-  },
-  {
-    title: 'Proyecto 3',
-    description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
-    technologies: ['Javascript', 'Tailwind CSS', 'MySQL', 'MySQL', 'MySQL', 'MySQL', 'MySQL'],
-    liveUrl: 'https://proyecto2.com',
-    githubUrl: 'https://github.com/usuario/proyecto2'
+    liveUrl: '',
+    githubUrl: ''
   }
 ];
