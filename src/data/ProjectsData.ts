d9378fe7532a03ab71ba7676681fc2c757d427b1
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-01-10 16:47:46 -0300

    Add ProjectsData.ts and adjusted it to be a data source for Projects. We adjusted some aspects and order of icons on the page

---
+++ src/data/ProjectsData.ts
@@ -0,0 +1,30 @@
+export const projectsData = [
+  {
+    title: 'Mini CRM',
+    description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
+    technologies: ['PHP', 'Laravel', 'Javascript' ,'MySQL', 'Vite'],
+    liveUrl: 'https://proyecto1.com',
+    githubUrl: 'https://github.com/usuario/proyecto1'
+  },
+  {
+    title: 'Proyecto 2',
+    description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
+    technologies: ['Vue.js', 'Express', 'PostgreSQL'],
+    liveUrl: 'https://proyecto2.com',
+    githubUrl: 'https://github.com/usuario/proyecto2'
+  },
+  {
+    title: 'Proyecto 3',
+    description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
+    technologies: ['Javascript', 'Tailwind CSS', 'MySQL', 'MySQL', 'MySQL', 'MySQL', 'MySQL'],
+    liveUrl: 'https://proyecto2.com',
+    githubUrl: 'https://github.com/usuario/proyecto2'
+  },
+  {
+    title: 'Proyecto 3',
+    description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
+    technologies: ['Javascript', 'Tailwind CSS', 'MySQL', 'MySQL', 'MySQL', 'MySQL', 'MySQL'],
+    liveUrl: 'https://proyecto2.com',
+    githubUrl: 'https://github.com/usuario/proyecto2'
+  }
+];
