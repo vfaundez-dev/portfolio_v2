9672c3a2dd411ca799429a2615745e9dcc6c2bb2
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-01-12 20:04:25 -0300

    Ajust list of projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: netlify()
 });
--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -8,22 +8,8 @@ export const projectsData = [
   },
   {
     title: 'Mini CRM',
-    description: 'En etapa de finalizacion...',
+    description: 'En fase de finalizacion...',
     technologies: ['PHP', 'Laravel', 'Javascript' ,'MySQL', 'Vite', 'AdminLTE'],
-    liveUrl: '',
-    githubUrl: ''
-  },
-  {
-    title: 'Proyecto 3',
-    description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
-    technologies: ['Vue.js', 'Express', 'PostgreSQL'],
-    liveUrl: '',
-    githubUrl: ''
-  },
-  {
-    title: 'Proyecto 4',
-    description: 'Nisi enim cillum culpa minim quis anim occaecat veniam anim adipisicing excepteur qui aliquip Lorem. Ea excepteur voluptate id magna sunt ea. Exercitation ipsum excepteur dolor non nulla. Officia id ea consequat exercitation enim ad est qui consectetur aliqua. Est mollit nulla magna officia veniam. Et elit mollit labore est ad sunt in proident.',
-    technologies: ['Javascript', 'Tailwind CSS', 'MySQL', 'MySQL', 'MySQL', 'MySQL', 'MySQL'],
     liveUrl: '',
     githubUrl: ''
   }
